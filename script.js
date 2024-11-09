6db14bdebab888da4e1672c285bee335419d4825
Arun <arunneupane0000@gmail.com>
2024-11-09 16:11:07 +0545

    feat : email linked to contact to get message directly in my email

--- script.js
+++ script.js
@@ -230,7 +230,28 @@ function toggleMenu() {
   const navLinks = document.querySelector(".nav-links");
   navLinks.classList.toggle("active");
   document.querySelector(".menu-toggle").classList.toggle("active");
+
+  if (navLinks.classList.contains("active")) {
+    setTimeout(() => {
+      document.addEventListener("click", handleOutsideClick);
+    }, 1000);
+  } else {
+    document.removeEventListener("click", handleOutsideClick);
+  }
 }
+
+function handleOutsideClick(event) {
+  const navLinks = document.querySelector(".nav-links");
+  const menuToggle = document.querySelector(".menu-toggle");
+
+  if (!navLinks.contains(event.target)) {
+    navLinks.classList.remove("active");
+    menuToggle.classList.remove("active");
+
+    document.removeEventListener("click", handleOutsideClick);
+  }
+}
+
 function randomGenerate() {
   if (sortBtn.textContent == "Sorting...") return;
   const random = Math.floor(Math.random() * 10) + 5;
